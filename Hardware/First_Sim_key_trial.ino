03c8b100351b952404f74ebfda573b0abeffe7d7
chetan <chetan249581@gmail.com>
2023-12-29 18:55:18 +0530

    Modified Simulation code

--- Hardware/First_Sim_key_trial.ino
+++ Hardware/First_Sim_key_trial.ino
@@ -1,5 +1,5 @@
+
 #include <Keypad.h>
-#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
  
 #define ROWS  4
@@ -12,8 +12,8 @@ char keyMap[ROWS][COLS] = {
   {'*','0','#', 'D'}
 };
  
-uint8_t rowPins[ROWS] = {0, 2, 14, 12}; 
-uint8_t colPins[COLS] = {13, 15, 3, 1}; 
+uint8_t rowPins[ROWS] = {14,27,26,25}; 
+uint8_t colPins[COLS] = {33,32,18,19}; 
 uint8_t LCD_CursorPosition = 0;
 String P1 = "111";
 String P2 = "222";
@@ -28,7 +28,7 @@ Keypad keypad = Keypad(makeKeymap(keyMap), rowPins, colPins, ROWS, COLS );
 LiquidCrystal_I2C I2C_LCD(0x27, 16, 2);  
  
 void setup(){
-  Serial.begin(115200);
+  // Serial.begin(115200);
   I2C_LCD.init();
   I2C_LCD.backlight();
   I2C_LCD.clear();
